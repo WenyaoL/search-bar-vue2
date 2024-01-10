2e1230f37083ea8846c605e1a890035b7c889253
yinjie.chen <1761038183@qq.com>
2024-01-10 10:22:15 +0800

    feature:修改配置信息

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import Vue,{VueConstructor} from 'vue'
-import SearchBar from './components/SearchBar.vue'
+import Vue, { VueConstructor } from "vue";
+import SearchBar from "./components/searchBar.vue";
 
-const install = (Vue:VueConstructor,options:any) => {
-    Vue.component('SearchBar',SearchBar)
-}
+const install = (Vue: VueConstructor, options: any) => {
+  Vue.component("SearchBar", SearchBar);
+};
 
-export default {install,SearchBar}
+export default { install, SearchBar };
 
-export {install,SearchBar}
+export { install, SearchBar };
